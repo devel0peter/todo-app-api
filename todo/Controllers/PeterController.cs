639ee42a137b54690cfd5d0bf9633398aedb59b4
Peter Acs <peter.acs@panoramic.systems>
2018-11-08 09:43:47 +0000

    Add PeterController to test routing

---
+++ todo/Controllers/PeterController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace todo.Controllers
+{
+	[Route("peter")]
+	public class PeterController : Controller
+	{
+		// GET
+		[HttpGet("hello")]
+		[HttpGet("hi")]
+		public IActionResult SayHello()
+		{
+			return Content("Hello World!");
+		}
+
+		[HttpGet("hi/{name}", Name = "hi_name")]
+		public IActionResult SayHello(string name)
+		{
+			return Content($"Hello {name}!");
+		}
+
+		[HttpGet("data")]
+		public ActionResult<Info> ReturnData()
+		{
+			Info info = new Info
+			{
+				Name = "Carolin",
+				Age = 28
+			};
+
+			return info;
+		}
+	}
+
+	public class Info
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
